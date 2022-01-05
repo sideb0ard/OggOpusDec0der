b439a16090eeddfc363dc318334289d12c71a5bf
Thorsten Sideboard <thorsten.sideb0ard@gmail.com>
2022-01-05 13:23:19 -0800

    onto the actual decoding - might need to go use libopus after all.

--- oofd.cpp
+++ oofd.cpp
@@ -27,6 +27,9 @@
 
 const std::string testfile("gaborNote.opus");
 
+////////////////////////////////////////////////////////////
+// CONST VARS
+
 // Ogg Header Magic Number
 const std::string kOggCapturePattern{"OggS"};
 
@@ -37,6 +40,8 @@ const std::string kOpusCommentHeader{"OpusTags"};
 // variable header size, based on number of segments.
 const int kMinHeaderSize = 27;
 const int kPageSegmentField = kMinHeaderSize - 1;
+
+////////////////////////////////////////////////////////////
 
 namespace {
 // searches for pattern string in the buffer reference starting at buffer_idx.
@@ -54,6 +59,61 @@ bool MatchesPattern(const std::string& pattern,
 }
 
 }  // namespace
+
+////////////////////////////////////////////////////////////
+
+bool DecodeIDHeader(const std::vector<unsigned char>& buffer, int buffer_idx) {
+  buffer_idx += kOpusIdHeader.length();  // skip header
+  unsigned int bytes_remaining = buffer.size() - buffer_idx;
+  if (bytes_remaining > 1) {
+    int version = buffer[buffer_idx];
+    std::cout << "Version:" << version << std::endl;
+
+    buffer_idx += 1;
+    bytes_remaining = buffer.size() - buffer_idx;
+
+    if (bytes_remaining > 1) {
+      int num_channels = buffer[buffer_idx];
+      std::cout << "Num channels:" << num_channels << std::endl;
+      buffer_idx += 1;
+      bytes_remaining = buffer.size() - buffer_idx;
+
+      if (bytes_remaining > 2) {
+        uint16_t pre_skip = buffer[buffer_idx] | (buffer[buffer_idx + 1] << 8);
+        std::cout << "PRESKIP:" << pre_skip << std::endl;
+
+        buffer_idx += 2;
+        bytes_remaining = buffer.size() - buffer_idx;
+
+        if (bytes_remaining > 4) {
+          uint32_t input_sample_rate =
+              buffer[buffer_idx] | (buffer[buffer_idx + 1] << 8) |
+              (buffer[buffer_idx + 2] << 16) | (buffer[buffer_idx + 3] << 24);
+          std::cout << "INPUT SAMPLE RATE:" << input_sample_rate << std::endl;
+          buffer_idx += 4;
+          bytes_remaining = buffer.size() - buffer_idx;
+
+          if (bytes_remaining > 2) {
+            uint16_t output_gain =
+                buffer[buffer_idx] | (buffer[buffer_idx + 1] << 8);
+            std::cout << "OUTPUT GAIN:" << output_gain << std::endl;
+            buffer_idx += 2;
+            bytes_remaining = buffer.size() - buffer_idx;
+
+            if (bytes_remaining > 1) {
+              uint8_t channel_mapping_family = buffer[buffer_idx];
+              std::cout << "CHANNEL MAPPING FAM:" << channel_mapping_family
+                        << std::endl;
+
+              return true;
+            }
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
 
 bool DecodeComments(const std::vector<unsigned char>& buffer, int buffer_idx) {
   buffer_idx += kOpusCommentHeader.length();  // skip header
@@ -118,12 +178,19 @@ bool DecodeComments(const std::vector<unsigned char>& buffer, int buffer_idx) {
   return false;
 }
 
-void DecodeOggPage(int page_num, std::vector<unsigned char> page) {
-  // https://datatracker.ietf.org/doc/html/rfc7845
-  // page #0 should only contain ID Header
-  // page #1..n should only contain comments
-  // page #(n+1)... onwards should be Audio Data Packets
+/////////////////////////////////////////////////////////////////////////
+
+bool DecodeOpusPacket(const std::vector<unsigned char>& buffer,
+                      int buffer_idx) {
+  int bytes_remaining = buffer.size() - buffer_idx;
+  if (bytes_remaining > 1) {
+    return true;
+  }
+
+  return false;
 }
+
+/////////////////////////////////////////////////////////////////////////
 
 void DecodeOggStream(std::vector<unsigned char>& buffer) {
   int idx = 0;
@@ -183,7 +250,11 @@ void DecodeOggStream(std::vector<unsigned char>& buffer) {
         if (is_first_page_of_bitstream && current_page == 0 &&
             MatchesPattern(kOpusIdHeader, buffer, data_start_idx)) {
           std::cout << "**HEADER IDX!\n";
-          have_header_page = true;
+
+          if (DecodeIDHeader(buffer, data_start_idx)) {
+            have_header_page = true;
+          }
+
         } else if (have_header_page &&
                    MatchesPattern(kOpusCommentHeader, buffer, data_start_idx)) {
           std::cout << "**COMMENT IDX!\n";
@@ -192,6 +263,8 @@ void DecodeOggStream(std::vector<unsigned char>& buffer) {
           }
         } else if (have_comment_page) {  // data pages
           std::cout << "**DATA PAGES!\n";
+          if (DecodeOpusPacket(buffer, data_start_idx)) {
+          }
         }
 
         idx += total_page_size;
@@ -205,6 +278,8 @@ void DecodeOggStream(std::vector<unsigned char>& buffer) {
   }
   std::cout << "\nDone! IDX is at:" << idx << std::endl;
 }
+
+////////////////////////////////////////////////////////////
 
 int main(int argc, char** argv) {
   std::string infile = testfile;
